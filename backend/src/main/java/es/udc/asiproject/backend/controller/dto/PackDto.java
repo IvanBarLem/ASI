4f3f75d7b79ca08317213c0259b9812ba2d32d57
David García Gondell <david.ggondell@udc.es>
2021-10-21 12:52:30 +0200

    description longer tha 60

--- backend/src/main/java/es/udc/asiproject/backend/controller/dto/PackDto.java
+++ backend/src/main/java/es/udc/asiproject/backend/controller/dto/PackDto.java
@@ -13,160 +13,160 @@ import javax.validation.constraints.Size;
 import es.udc.asiproject.backend.controller.dto.validation.InsertValidation;
 
 public class PackDto {
-	private Long id;
-	@NotBlank(groups = { InsertValidation.class })
-	@Size(min = 1, max = 60, groups = { InsertValidation.class })
-	private String title;
-	@NotBlank(groups = { InsertValidation.class })
-	@Size(min = 1, max = 60, groups = { InsertValidation.class })
-	private String description;
-	@NotEmpty(groups = { InsertValidation.class })
-	private String image;
-	@NotNull(groups = { InsertValidation.class })
-	@Positive(groups = { InsertValidation.class })
-	private BigDecimal price;
-	@NotNull(groups = { InsertValidation.class })
-	@Positive(groups = { InsertValidation.class })
-	private Short duration;
-	@NotBlank(groups = { InsertValidation.class })
-	@Size(min = 1, max = 60, groups = { InsertValidation.class })
-	private String persons;
-	@NotNull(groups = { InsertValidation.class })
-	private Set<AccommodationDto> accommodations;
-	@NotNull(groups = { InsertValidation.class })
-	private Set<ActivityDto> activities;
-	@NotNull(groups = { InsertValidation.class })
-	private Set<TransportDto> transports;
-	@NotNull(groups = { InsertValidation.class })
-	private Set<TravelDto> travels;
+    private Long id;
+    @NotBlank(groups = { InsertValidation.class })
+    @Size(min = 1, max = 60, groups = { InsertValidation.class })
+    private String title;
+    @NotBlank(groups = { InsertValidation.class })
+    @Size(min = 1, max = 500, groups = { InsertValidation.class })
+    private String description;
+    @NotEmpty(groups = { InsertValidation.class })
+    private String image;
+    @NotNull(groups = { InsertValidation.class })
+    @Positive(groups = { InsertValidation.class })
+    private BigDecimal price;
+    @NotNull(groups = { InsertValidation.class })
+    @Positive(groups = { InsertValidation.class })
+    private Short duration;
+    @NotBlank(groups = { InsertValidation.class })
+    @Size(min = 1, max = 60, groups = { InsertValidation.class })
+    private String persons;
+    @NotNull(groups = { InsertValidation.class })
+    private Set<AccommodationDto> accommodations;
+    @NotNull(groups = { InsertValidation.class })
+    private Set<ActivityDto> activities;
+    @NotNull(groups = { InsertValidation.class })
+    private Set<TransportDto> transports;
+    @NotNull(groups = { InsertValidation.class })
+    private Set<TravelDto> travels;
 
-	public PackDto() {
-	}
+    public PackDto() {
+    }
 
-	public PackDto(Long id, String title, String description, String image, BigDecimal price, Short duration,
-			String persons, Set<AccommodationDto> accommodations, Set<ActivityDto> activities,
-			Set<TransportDto> transports, Set<TravelDto> travels) {
-		this.id = id;
-		this.title = title.trim();
-		this.description = description.trim();
-		this.image = image;
-		this.price = price;
-		this.duration = duration;
-		this.persons = persons.trim();
-		this.accommodations = accommodations;
-		this.activities = activities;
-		this.transports = transports;
-		this.travels = travels;
-	}
+    public PackDto(Long id, String title, String description, String image, BigDecimal price, Short duration,
+	    String persons, Set<AccommodationDto> accommodations, Set<ActivityDto> activities,
+	    Set<TransportDto> transports, Set<TravelDto> travels) {
+	this.id = id;
+	this.title = title.trim();
+	this.description = description.trim();
+	this.image = image;
+	this.price = price;
+	this.duration = duration;
+	this.persons = persons.trim();
+	this.accommodations = accommodations;
+	this.activities = activities;
+	this.transports = transports;
+	this.travels = travels;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+	return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+	this.id = id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+	return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+	this.title = title;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+	return image;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage(String image) {
+	this.image = image;
+    }
 
-	public BigDecimal getPrice() {
-		return price;
-	}
+    public BigDecimal getPrice() {
+	return price;
+    }
 
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
+    public void setPrice(BigDecimal price) {
+	this.price = price;
+    }
 
-	public Short getDuration() {
-		return duration;
-	}
+    public Short getDuration() {
+	return duration;
+    }
 
-	public void setDuration(Short duration) {
-		this.duration = duration;
-	}
+    public void setDuration(Short duration) {
+	this.duration = duration;
+    }
 
-	public String getPersons() {
-		return persons;
-	}
+    public String getPersons() {
+	return persons;
+    }
 
-	public void setPersons(String persons) {
-		this.persons = persons;
-	}
+    public void setPersons(String persons) {
+	this.persons = persons;
+    }
 
-	public Set<AccommodationDto> getAccommodations() {
-		return accommodations;
-	}
+    public Set<AccommodationDto> getAccommodations() {
+	return accommodations;
+    }
 
-	public void setAccommodations(Set<AccommodationDto> accommodations) {
-		this.accommodations = accommodations;
-	}
+    public void setAccommodations(Set<AccommodationDto> accommodations) {
+	this.accommodations = accommodations;
+    }
 
-	public Set<ActivityDto> getActivities() {
-		return activities;
-	}
+    public Set<ActivityDto> getActivities() {
+	return activities;
+    }
 
-	public void setActivities(Set<ActivityDto> activities) {
-		this.activities = activities;
-	}
+    public void setActivities(Set<ActivityDto> activities) {
+	this.activities = activities;
+    }
 
-	public Set<TransportDto> getTransports() {
-		return transports;
-	}
+    public Set<TransportDto> getTransports() {
+	return transports;
+    }
 
-	public void setTransports(Set<TransportDto> transports) {
-		this.transports = transports;
-	}
+    public void setTransports(Set<TransportDto> transports) {
+	this.transports = transports;
+    }
 
-	public Set<TravelDto> getTravels() {
-		return travels;
-	}
+    public Set<TravelDto> getTravels() {
+	return travels;
+    }
 
-	public void setTravels(Set<TravelDto> travels) {
-		this.travels = travels;
-	}
+    public void setTravels(Set<TravelDto> travels) {
+	this.travels = travels;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(accommodations, activities, description, duration, id, image, persons, price, title,
-				transports, travels);
-	}
+    @Override
+    public int hashCode() {
+	return Objects.hash(accommodations, activities, description, duration, id, image, persons, price, title,
+		transports, travels);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PackDto other = (PackDto) obj;
-		return Objects.equals(accommodations, other.accommodations) && Objects.equals(activities, other.activities)
-				&& Objects.equals(description, other.description) && Objects.equals(duration, other.duration)
-				&& Objects.equals(id, other.id) && Objects.equals(image, other.image)
-				&& Objects.equals(persons, other.persons) && Objects.equals(price, other.price)
-				&& Objects.equals(title, other.title) && Objects.equals(transports, other.transports)
-				&& Objects.equals(travels, other.travels);
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	PackDto other = (PackDto) obj;
+	return Objects.equals(accommodations, other.accommodations) && Objects.equals(activities, other.activities)
+		&& Objects.equals(description, other.description) && Objects.equals(duration, other.duration)
+		&& Objects.equals(id, other.id) && Objects.equals(image, other.image)
+		&& Objects.equals(persons, other.persons) && Objects.equals(price, other.price)
+		&& Objects.equals(title, other.title) && Objects.equals(transports, other.transports)
+		&& Objects.equals(travels, other.travels);
+    }
 }
--- backend/src/main/java/es/udc/asiproject/backend/service/impl/PackServiceImpl.java
+++ backend/src/main/java/es/udc/asiproject/backend/service/impl/PackServiceImpl.java
@@ -27,72 +27,72 @@ import es.udc.asiproject.backend.service.exceptions.InvalidOperationException;
 
 @Service
 public class PackServiceImpl implements PackService {
-	@Autowired
-	AccommodationDao accommodationDao;
-	@Autowired
-	ActivityDao activityDao;
-	@Autowired
-	TransportDao transportDao;
-	@Autowired
-	TravelDao travelDao;
-	@Autowired
-	PackDao packDao;
+    @Autowired
+    AccommodationDao accommodationDao;
+    @Autowired
+    ActivityDao activityDao;
+    @Autowired
+    TransportDao transportDao;
+    @Autowired
+    TravelDao travelDao;
+    @Autowired
+    PackDao packDao;
 
-	@Override
-	@Transactional
-	public Pack createPack(Pack pack) throws InstanceNotFoundException, InvalidOperationException {
-		if (pack.getAccommodations().isEmpty() && pack.getActivities().isEmpty() && pack.getTransports().isEmpty()
-				&& pack.getTravels().isEmpty()) {
-			throw new InvalidOperationException("EmptyPack");
-		}
-
-		for (Accommodation accommodation : pack.getAccommodations()) {
-			Optional<Accommodation> optional = accommodationDao.findById(accommodation.getId());
-
-			if (optional.isPresent()) {
-				accommodation.setName(optional.get().getName());
-			} else {
-				throw new InstanceNotFoundException(Accommodation.class.getSimpleName(), accommodation.getId());
-			}
-		}
-		for (Activity activity : pack.getActivities()) {
-			Optional<Activity> optional = activityDao.findById(activity.getId());
-
-			if (optional.isPresent()) {
-				activity.setName(optional.get().getName());
-			} else {
-				throw new InstanceNotFoundException(Activity.class.getSimpleName(), activity.getId());
-			}
-		}
-		for (Transport transport : pack.getTransports()) {
-			Optional<Transport> optional = transportDao.findById(transport.getId());
-
-			if (optional.isPresent()) {
-				transport.setName(optional.get().getName());
-			} else {
-				throw new InstanceNotFoundException(Transport.class.getSimpleName(), transport.getId());
-			}
-		}
-		for (Travel travel : pack.getTravels()) {
-			Optional<Travel> optional = travelDao.findById(travel.getId());
-
-			if (optional.isPresent()) {
-				travel.setName(optional.get().getName());
-			} else {
-				throw new InstanceNotFoundException(Travel.class.getSimpleName(), travel.getId());
-			}
-		}
-
-		pack.setCreatedAt(new Date());
-
-		return packDao.save(pack);
+    @Override
+    @Transactional
+    public Pack createPack(Pack pack) throws InstanceNotFoundException, InvalidOperationException {
+	if (pack.getAccommodations().isEmpty() && pack.getActivities().isEmpty() && pack.getTransports().isEmpty()
+		&& pack.getTravels().isEmpty()) {
+	    throw new InvalidOperationException("EmptyPack");
 	}
 
-	@Override
-	@Transactional(readOnly = true)
-	public Page<Pack> findPacks(Integer pageNumber, Integer pageSize) {
-		Pageable pageable = PageRequest.of(pageNumber, pageSize, Direction.DESC, "createdAt");
+	for (Accommodation accommodation : pack.getAccommodations()) {
+	    Optional<Accommodation> optional = accommodationDao.findById(accommodation.getId());
 
-		return packDao.findAll(pageable);
+	    if (optional.isPresent()) {
+		accommodation.setName(optional.get().getName());
+	    } else {
+		throw new InstanceNotFoundException(Accommodation.class.getSimpleName(), accommodation.getId());
+	    }
 	}
+	for (Activity activity : pack.getActivities()) {
+	    Optional<Activity> optional = activityDao.findById(activity.getId());
+
+	    if (optional.isPresent()) {
+		activity.setName(optional.get().getName());
+	    } else {
+		throw new InstanceNotFoundException(Activity.class.getSimpleName(), activity.getId());
+	    }
+	}
+	for (Transport transport : pack.getTransports()) {
+	    Optional<Transport> optional = transportDao.findById(transport.getId());
+
+	    if (optional.isPresent()) {
+		transport.setName(optional.get().getName());
+	    } else {
+		throw new InstanceNotFoundException(Transport.class.getSimpleName(), transport.getId());
+	    }
+	}
+	for (Travel travel : pack.getTravels()) {
+	    Optional<Travel> optional = travelDao.findById(travel.getId());
+
+	    if (optional.isPresent()) {
+		travel.setName(optional.get().getName());
+	    } else {
+		throw new InstanceNotFoundException(Travel.class.getSimpleName(), travel.getId());
+	    }
+	}
+
+	pack.setCreatedAt(new Date());
+
+	return packDao.save(pack);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Pack> findPacks(Integer pageNumber, Integer pageSize) {
+	Pageable pageable = PageRequest.of(pageNumber, pageSize, Direction.DESC, "createdAt");
+
+	return packDao.findAll(pageable);
+    }
 }
